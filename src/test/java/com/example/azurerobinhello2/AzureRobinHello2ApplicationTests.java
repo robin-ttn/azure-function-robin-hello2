939170666584e1aeb0c720a83286996434fa2605
Robin <robin.rawat@tothenew.com>
2023-01-14 16:41:00 +0530

    Initial commit

---
+++ src/test/java/com/example/azurerobinhello2/AzureRobinHello2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurerobinhello2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureRobinHello2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
